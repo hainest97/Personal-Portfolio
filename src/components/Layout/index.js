1e8de50288d5fcb3e863c7dfdf5222fa96f0a5d5
tisthes <thaines@altny.net>
2022-10-15 15:35:05 -0400

    Installed important packages

---
+++ src/components/Layout/index.js
@@ -0,0 +1,10 @@
+import NavBar from '../Navbar'
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <NavBar/>
+  )
+}
+
+export default Layout
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import './index.scss'
+import {NavLink,Link} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelope,faHome,faUser} from '@fortawesome/react-fontawesome'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <p>PlaceHolder</p>
+    </nav>
+  )
+  
+}
+
+export default NavBar
